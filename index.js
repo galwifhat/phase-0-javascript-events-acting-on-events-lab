5ddc16a1debbfeb1062e5248c8834c36e3fbef9b
Ruth <cirurue@gmail.com>
2025-04-01 22:32:59 +0300

    events done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left.replace("px", ""), 10);
+
+  if (left > 0) {
+    // Prevent moving beyond left boundary
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left.replace("px", ""), 10);
+
+  if (left < 360) {
+    // Prevent moving beyond right boundary
+    dodger.style.left = `${left + 10}px`;
+  }
+}
